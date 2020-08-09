dbc6cf7c18e6b7369bcfae381f1837be6ac46bf7
AQA-PC-01 <AQA-PC-01@AAQA-PC-01>
2020-08-09 11:25:33 +0600

    Add project files.

---
+++ ConsolePractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            T1 t1 = new T1();
+            Console.WriteLine(t1.Name);
+        }
+    }
+}
---
+++ ConsolePractice/T1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePractice
+{
+    public class T1
+    {
+        private string name = "zahid";
+
+        public string Name 
+        {
+            get { return name; }
+            set { name = value; } 
+        }
+    }
+}
